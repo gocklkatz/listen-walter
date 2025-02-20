c23461afccc9da28af460b888fa1584f42e6b4e5
Stefan Katzensteiner <stefan.katzensteiner@mailbox.org>
2025-02-20 14:18:06 +0100

    Add new list input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,18 +4,19 @@ import { HeaderComponent } from './header/header.component';
 import { ListComponent } from './list/list.component';
 import { DUMMY_LIST } from './dummy-lists';
 import { TasksComponent } from "./tasks/tasks.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HeaderComponent, ListComponent, TasksComponent]
+  imports: [HeaderComponent, ListComponent, TasksComponent, FormsModule]
 })
 export class AppComponent {
-
   lists = DUMMY_LIST;
   selectedListId?: string;
+  newListName = '';
 
   get selectedList() {
     return this.lists.find((list) => list.id === this.selectedListId);
@@ -23,5 +24,15 @@ export class AppComponent {
 
   onSelectList(id: string) {
     this.selectedListId = id;
+  }
+
+  onAddList() {
+    if (this.newListName) {
+      this.lists.push({
+        id: String(this.lists.length + 1),
+        name: this.newListName
+      });
+      this.newListName = '';
+    }
   }
 }
